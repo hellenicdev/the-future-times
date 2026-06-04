f58c6a7641a225e891bfe75f03d3a509caf68406
hellenicdev <ikonpanos@gmail.com>
2026-06-04 16:15:29 +0300

    Update Render URL in config

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ const API_URL = (() => {
   if (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') {
     return 'http://localhost:3000/api'
   }
-  return 'https://future-times.onrender.com/api'
+  return 'https://the-future-times.onrender.com/api'
 })()
 
 const TURNSTILE_SITE_KEY = '0x4AAAAAADewRHb-jdln-Mn1'
